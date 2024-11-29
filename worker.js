fb95a089cb0292f19455632601e2691055126fbc
theking12b <pars.atarkash10@gmail.com>
2024-11-29 23:59:15 +0330

    4

--- worker.js
+++ worker.js
@@ -1,8 +1,15 @@
 export default {
     async fetch(request, env, ctx) {
-                     const url = new URL(request.url);
-                     url.hostname = env.HOST || 'www.speedtest.net';
-                     url.protocol = 'https:';
-                     request = new Request(url, request);
-                     return fetch(request);
-           }
+        const url = new URL(request.url);
+        url.hostname = env.HOST || 'www.speedtest.net';
+        url.protocol = 'https:';
+        const modifiedRequest = new Request(url, request);
+        const response = await fetch(modifiedRequest);
+
+        // Log response status and headers for debugging
+        console.log("Response Status:", response.status);
+        console.log("Response Headers:", [...response.headers.entries()]);
+        
+        return response;
+    }
+}
