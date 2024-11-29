a2f8307a364f5e11004dc25ddd49fb7da0dc8313
theking12b <pars.atarkash10@gmail.com>
2024-11-30 00:05:54 +0330

    6

--- worker.js
+++ worker.js
@@ -1,7 +1,15 @@
 export default {
     async fetch(request, env, ctx) {
-        return new Response("Hello, World!", {
-            headers: { "Content-Type": "text/plain" },
-        });
+        const url = new URL(request.url);
+        url.hostname = env.HOST || 'www.speedtest.net';
+        url.protocol = 'https:';
+        const modifiedRequest = new Request(url, request);
+        const response = await fetch(modifiedRequest);
+
+        // Log response status and headers for debugging
+        console.log("Response Status:", response.status);
+        console.log("Response Headers:", [...response.headers.entries()]);
+        
+        return response;
     }
 }
